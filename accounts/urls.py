c1f9f8ce50fa7fdca32d5b8d7a6cb516a466bc1d
jhs512 <jangka512@gmail.com>
2022-02-09 12:40:20 +0900

    이모지 활성화

--- accounts/urls.py
+++ accounts/urls.py
@@ -29,7 +29,8 @@ urlpatterns = [
     ), name='reset_password'),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(
-        success_url = reverse_lazy('accounts:password_reset_complete')
+        success_url=reverse_lazy('accounts:password_reset_complete'),
+        template_name='accounts/password_reset_confirm.html'
     ), name='password_reset_confirm'),
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
--- base/settings/common.py
+++ base/settings/common.py
@@ -99,7 +99,9 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",  # 추가, 만약에 이 부분 때문에 오류가 난다면, 이 라인을 지우고 다시 시도해주세요.
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+            'use_unicode': True,
         },
     }
 }
