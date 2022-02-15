b0dc7af0a7f51ab6f9b59203bd85c66730db9dd5
jhs512 <jangka512@gmail.com>
2022-02-16 07:37:56 +0900

    배포

--- git_pull_and_rerun.sh
+++ git_pull_and_rerun.sh
@@ -3,15 +3,9 @@
 # 테스트 환경에서 테스트 먼저
 {
   docker start python__2__test
-  docker exec python__2__test bash -c "cd /data/site_projects/python__2__test/src/ ; git pull origin master"
-  docker exec python__2__test bash -c "cd /data/site_projects/python__2__test/src/ ; pip install -r requirements/prod.txt"
-  OUTPUT_TEST=$(docker exec python__2__test bash -c "cd /data/site_projects/python__2__test/src/ ; python manage.py test -v 2 --settings=base.settings.prod 2>&1")
-  echo $OUTPUT_TEST
-
-  if [[ $OUTPUT_TEST == *"FAILED"* ]]; then
-    echo "HI"
-    exit 1
-  fi
+  docker exec python__2__test bash -e "cd /data/site_projects/python__2__test/src/ ; git pull origin master"
+  docker exec python__2__test bash -e "cd /data/site_projects/python__2__test/src/ ; pip install -r requirements/prod.txt"
+  docker exec python__2__test bash -e "cd /data/site_projects/python__2__test/src/ ; python manage.py test -v 2 --settings=base.settings.prod 2>&1"
 } || {
   docker stop python__2__test
   exit 1
@@ -24,18 +18,18 @@ docker stop python__2__test
 docker exec python__2 pkill "gunicorn"
 
 # 폴더에 깃에 있는 최신소스코드 가져오기
-docker exec python__2 bash -c "cd /data/site_projects/python__2/src/ ; git pull origin master"
+docker exec python__2 bash -e "cd /data/site_projects/python__2/src/ ; git pull origin master"
 
 # 의존성 설치
-docker exec python__2 bash -c "cd /data/site_projects/python__2/src/ ; pip install -r requirements/prod.txt"
+docker exec python__2 bash -e "cd /data/site_projects/python__2/src/ ; pip install -r requirements/prod.txt"
 
 # 마이그레이트
-docker exec python__2 bash -c "cd /data/site_projects/python__2/src/ ; python manage.py migrate --settings=base.settings.prod"
+docker exec python__2 bash -e "cd /data/site_projects/python__2/src/ ; python manage.py migrate --settings=base.settings.prod"
 
 # 장고를 운영모드로 실행
-docker exec python__2 bash -c "cd /data/site_projects/python__2/src ; nohup gunicorn --bind=0.0.0.0:8000 base.wsgi &"
+docker exec python__2 bash -e "cd /data/site_projects/python__2/src ; nohup gunicorn --bind=0.0.0.0:8000 base.wsgi &"
 
 # static collect 다시 수행
-docker exec python__2 bash -c "cd /data/site_projects/python__2/src ; echo yes | python manage.py collectstatic --settings=base.settings.prod"
+docker exec python__2 bash -e "cd /data/site_projects/python__2/src ; echo yes | python manage.py collectstatic --settings=base.settings.prod"
 
 exit 0
