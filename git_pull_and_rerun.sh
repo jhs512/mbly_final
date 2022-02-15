3f03601784454ea9797eba10775b58b3f630dfcd
jhs512 <jangka512@gmail.com>
2022-02-16 07:40:47 +0900

    배포

--- git_pull_and_rerun.sh
+++ git_pull_and_rerun.sh
@@ -3,9 +3,9 @@
 # 테스트 환경에서 테스트 먼저
 {
   docker start python__2__test
-  docker exec python__2__test bash -e "cd /data/site_projects/python__2__test/src/ ; git pull origin master"
-  docker exec python__2__test bash -e "cd /data/site_projects/python__2__test/src/ ; pip install -r requirements/prod.txt"
-  docker exec python__2__test bash -e "cd /data/site_projects/python__2__test/src/ ; python manage.py test -v 2 --settings=base.settings.prod 2>&1"
+  docker exec python__2__test bash -ce "cd /data/site_projects/python__2__test/src/ ; git pull origin master"
+  docker exec python__2__test bash -ce "cd /data/site_projects/python__2__test/src/ ; pip install -r requirements/prod.txt"
+  docker exec python__2__test bash -ce "cd /data/site_projects/python__2__test/src/ ; python manage.py test -v 2 --settings=base.settings.prod 2>&1"
 } || {
   docker stop python__2__test
   exit 1
